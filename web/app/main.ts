dd6ed2c8d553350a31743157ba15f0047d3cd740
Remigiusz Modrzejewski <lrem@maxnet.org.pl>
2017-03-24 23:09:53 +0000

    Recreated project with current Angular.

--- web/app/main.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent, GameComponent} from './app.component'
-import {Http, Response, RequestOptions, Headers, Request, RequestMethod, HTTP_PROVIDERS} from 'angular2/http';
-
-
-bootstrap(GameComponent, [HTTP_PROVIDERS]);
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
