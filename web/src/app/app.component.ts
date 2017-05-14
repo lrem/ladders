d840cac92f75aadfb9c5589b2385733dd3f4e71f
Remigiusz Modrzejewski <git@lrem.net>
2017-05-14 16:45:37 +0100

    Linting.

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,39 +1,39 @@
 import { Component, Input, Output, Inject, ViewChild, NgZone } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {Http, Response, RequestOptions, Headers, Request, RequestMethod} from '@angular/http';
-import {MdDialog, MdDialogRef, MD_DIALOG_DATA, MdSnackBar} from '@angular/material';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Http, Response, RequestOptions, Headers, Request, RequestMethod } from '@angular/http';
+import { MdDialog, MdDialogRef, MD_DIALOG_DATA, MdSnackBar } from '@angular/material';
 import { Location } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'rxjs/add/operator/switchMap';
-import 'rxjs/Rx' ;
+import 'rxjs/Rx';
 
 @Component({
-    selector: 'game',
-    templateUrl: './game.component.html',
+  selector: 'game',
+  templateUrl: './game.component.html',
 })
 export class GameDialog {
-  public ladder : string;
+  public ladder: string;
   public submitting = false;
   public teams_count = 2;
   public players_per_team = 1;
-  public players = [[{name: ''}], [{name: ''}]];
+  public players = [[{ name: '' }], [{ name: '' }]];
   constructor(public http: Http,
-              public dialogRef: MdDialogRef<GameDialog>,
-              @Inject(MD_DIALOG_DATA) data:any) {
+    public dialogRef: MdDialogRef<GameDialog>,
+    @Inject(MD_DIALOG_DATA) data: any) {
     this.http = http;
     this.ladder = data.ladder;
   }
   onSubmit() {
     this.submitting = true;
-    let headers = new Headers();
+    const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    let result = JSON.stringify({'outcome': this.players});
+    const result = JSON.stringify({ 'outcome': this.players });
     console.debug(result);
     this.http.post(`http://127.0.0.1:5000/${this.ladder}/game`,
-              result, {headers: headers}).subscribe(() => {
-                this.dialogRef.close();
-              });
+      result, { headers: headers }).subscribe(() => {
+        this.dialogRef.close();
+      });
   }
 }
 
@@ -45,12 +45,12 @@ export class GameDialog {
   templateUrl: './ranking.component.html'
 })
 export class RankingComponent {
-  public ladder : string;
-  public ranking : Object;
-  public ready : boolean = false;
+  public ladder: string;
+  public ranking: Object;
+  public ready = false;
   public math = Math;
   constructor(private http: Http,
-             ) {}
+  ) { }
   ngOnInit() {
     this.reload()
   }
@@ -58,7 +58,7 @@ export class RankingComponent {
     this.http.get(`http://127.0.0.1:5000/${this.ladder}/ranking`).
       map(res => res.json()).
       subscribe(json => {
-        if(json.exists) {
+        if (json.exists) {
           this.ranking = json.ranking;
           this.ready = true
         } else {
@@ -76,12 +76,12 @@ export class RankingComponent {
   templateUrl: './matches.component.html',
 })
 export class MatchesComponent {
-  public ladder : string;
-  public matchList : Object;
-  public ready : boolean = false;
+  public ladder: string;
+  public matchList: Object;
+  public ready = false;
   public id_token;
   constructor(private http: Http,
-             ) {}
+  ) { }
   ngOnInit() {
     this.reload()
   }
@@ -89,7 +89,7 @@ export class MatchesComponent {
     this.http.get(`http://127.0.0.1:5000/${this.ladder}/matches`).
       map(res => res.json()).
       subscribe(json => {
-        if(json.exists) {
+        if (json.exists) {
           this.matchList = json.matches;
           this.ready = true
         } else {
@@ -99,31 +99,31 @@ export class MatchesComponent {
       });
   }
   remove(id) {
-    let headers = new Headers();
+    const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     this.http.post(`http://127.0.0.1:5000/${this.ladder}/remove`,
-                   {id: id, idtoken: this.id_token}, {headers: headers})
-      .subscribe(() => {this.reload();});
+      { id: id, idtoken: this.id_token }, { headers: headers })
+      .subscribe(() => { this.reload(); });
   }
 }
 
 @Component({
-    selector: 'ladder',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'ladder',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class LadderComponent {
   @ViewChild(RankingComponent) ranking: RankingComponent;
   @ViewChild(MatchesComponent) matches: MatchesComponent;
-  public ladder : string;
+  public ladder: string;
   public owned: boolean;
   public id_token;
   constructor(private http: Http,
-              private route:ActivatedRoute,
-              private location:Location,
-              private ngZone: NgZone,
-              public gameDialog: MdDialog,
-             ) {
+    private route: ActivatedRoute,
+    private location: Location,
+    private ngZone: NgZone,
+    public gameDialog: MdDialog,
+  ) {
     window['onSignIn'] =
       (googleUser) => ngZone.run(() => this.onSignIn(googleUser));
   }
@@ -131,20 +131,20 @@ export class LadderComponent {
     this.route.params.subscribe(params => this.ladder = params['ladder']);
   }
   openGameDialog() {
-    let dialogRef = this.gameDialog.open(GameDialog,
-                                         {data: {ladder: this.ladder}});
-    dialogRef.afterClosed().subscribe(result => {this.reload();});
+    const dialogRef = this.gameDialog.open(GameDialog,
+      { data: { ladder: this.ladder } });
+    dialogRef.afterClosed().subscribe(result => { this.reload(); });
   }
   onSignIn(googleUser) {
-    let id_token = googleUser.getAuthResponse().id_token;
+    const id_token = googleUser.getAuthResponse().id_token;
     this.id_token = id_token
-    let headers = new Headers();
+    const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     this.http.post(`http://127.0.0.1:5000/${this.ladder}/owned`,
-                   {idtoken: id_token}, {headers: headers})
-                   .map(res => res.json()).subscribe(json => {
-                     this.owned = json;
-                   });
+      { idtoken: id_token }, { headers: headers })
+      .map(res => res.json()).subscribe(json => {
+        this.owned = json;
+      });
   }
   reload() {
     this.ranking.reload();
@@ -167,21 +167,21 @@ export class CreateComponent {
   public draw_probability = 0;
   public submitting = false;
   constructor(private http: Http,
-              private ngZone: NgZone,
-              private snackBar: MdSnackBar,
-             ) {
+    private ngZone: NgZone,
+    private snackBar: MdSnackBar,
+  ) {
     window['onSignIn'] =
       (googleUser) => ngZone.run(() => this.onSignIn(googleUser));
   }
   onSignIn(googleUser) {
-    let id_token = googleUser.getAuthResponse().id_token;
+    const id_token = googleUser.getAuthResponse().id_token;
     this.id_token = id_token
   }
   onSubmit() {
     this.submitting = true;
-    let headers = new Headers();
+    const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    let settings = JSON.stringify({
+    const settings = JSON.stringify({
       name: this.ladder,
       mu: this.mu,
       sigma: this.sigma,
@@ -191,22 +191,22 @@ export class CreateComponent {
       idtoken: this.id_token,
     });
     this.http.post(`http://127.0.0.1:5000/${this.ladder}/create`,
-              settings, {headers: headers})
-              .toPromise()
-              .then(() => {
-                window.location.assign(`/ladder/${this.ladder}`);
-              })
-              .catch((error: any) => {
-                this.snackBar.open('Creation failed... Name already in use?',
-                                  'Dismiss', {duration: 5000});
-                this.submitting = false;
-              });
+      settings, { headers: headers })
+      .toPromise()
+      .then(() => {
+        window.location.assign(`/ladder/${this.ladder}`);
+      })
+      .catch((error: any) => {
+        this.snackBar.open('Creation failed... Name already in use?',
+          'Dismiss', { duration: 5000 });
+        this.submitting = false;
+      });
   }
 }
 
 @Component({
-    selector: 'app-root',
-    template: `<router-outlet></router-outlet>`,
-    //templateUrl: './app.component.html'
+  selector: 'app-root',
+  template: `<router-outlet></router-outlet>`,
+  //templateUrl: './app.component.html'
 })
 export class AppComponent { }
