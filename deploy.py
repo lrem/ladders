6f2f3b13a0710d7c2098452038dd056a14e50b53
Remigiusz Modrzejewski <lrem@maxnet.org.pl>
2017-06-17 09:51:19 +0100

    Fully operational prod deploy script, for #10.

--- deploy.py
+++ deploy.py
@@ -1,33 +1,62 @@
 #!/bin/env python3.6
 
+import logging
 import os
 import re
+import shutil
 import subprocess
 
-GIT_ROOT = "/home/lrem/git/ladders"
+GIT_ROOT = "/home/ladders/git/ladders"
+WEB_ROOT = "/home/ladders/web"
+API_ROOT = "/home/ladders/api"
+UWSGI_MASTER_PIPE = "/tmp/ladders.master"
 
 
-def checkout_and_check_signature() -> bool:
+def main() -> None:
+    logging.basicConfig()
+    logging.info("Checking out git.")
+    build_number = checkout_and_check_signature()
+    logging.info("Deploying frontend.")
+    deploy_web(build_number)
+    logging.info("Deploying backend.")
+    deploy_api(build_number)
+    logging.info("Done.")
+
+
+def checkout_and_check_signature() -> int:
     """Chdir to GIT_ROOT, checkout and verify if we should deploy."""
     os.chdir(GIT_ROOT)
     head = subprocess.run(
         ["git", "log", "--all", "--pretty=format:%G? %s", "-n", "1"],
         stdout=subprocess.PIPE, encoding="utf8").stdout
     match = re.match(r"N Artifacts from Travis build (\d+).", head)
-    if match is None:
-        return False
+    assert match is not None, "Not pushed by Travis build."
     branch = "build-" + match.group(1)
     assert subprocess.run(["git", "checkout", branch]).returncode == 0
     before = subprocess.run(
         ["git", "log", "--all", "--pretty=format:%G? %s", "-n", "1",
             "--skip", "1"],
         stdout=subprocess.PIPE, encoding="utf8").stdout
-    return before.startswith("G ")
+    assert before.startswith("G "), "Not signed with a trusted signature."
+    return int(match.group(1))
 
 
-def main() -> None:
-    if checkout_and_check_signature():
-        print("We should deploy.")
+def deploy_web(build_number: int) -> None:
+    """Move the old frontend away and copy the `dist` directory in place."""
+    if os.path.exists(WEB_ROOT):
+        os.rename(WEB_ROOT, WEB_ROOT + "-pre-%d" % build_number)
+    shutil.copytree(os.path.join(GIT_ROOT, "web", "dist"), WEB_ROOT)
+
+
+def deploy_api(build_number: int) -> None:
+    """Move the old file away, new one in place and reload uwsgi."""
+    script = os.path.join(API_ROOT, "api.py")
+    if os.path.exists(script):
+        os.rename(script, script+"pre-%d" % build_number)
+    shutil.copy2(os.path.join(GIT_ROOT, "api.py"), script)
+    pipe = open(UWSGI_MASTER_PIPE, "w")
+    pipe.write("r\n")
+    pipe.close()
 
 
 if __name__ == "__main__":
